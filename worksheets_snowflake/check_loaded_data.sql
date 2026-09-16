1f66aaaf9aaf08a0c848c2f400105e8038c0c5b6
Putu Indira Mahadiva Arysaputri <indiramahadiva1@gmail.com>
2026-09-16 09:49:08 +0200

    Load Netflix CSV to Snowflake staging with dlt

---
+++ worksheets_snowflake/check_loaded_data.sql
@@ -0,0 +1,17 @@
+USE ROLE movies_reader;
+USE DATABASE movies;
+
+SHOW SCHEMAS;
+
+USE SCHEMA staging;
+
+SHOW TABLES;
+
+USE WAREHOUSE dev_wh;
+SELECT * FROM netflix;
+
+DESC TABLE netflix;
+
+SELECT COUNT(*) FROM netflix;
+
+SHOW USERS LIKE 'EXTRACT_LOADER';
---
+++ worksheets_snowflake/setup_database.sql
@@ -0,0 +1,17 @@
+USE ROLE sysadmin;
+
+
+-- setup database and the staging layer 
+CREATE DATABASE IF NOT EXISTS movies;
+
+
+-- staging layer is a landing zone where data is loaded 
+CREATE SCHEMA IF NOT EXISTS movies.staging;
+
+DESCRIBE DATABASE movies;
+
+
+
+
+
+
